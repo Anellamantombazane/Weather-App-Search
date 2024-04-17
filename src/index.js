d36ff613429d8fccd79b348d7f2060a9228d8f28
Anellamantombazane <pnorah04@gmail.com>
2024-04-17 11:15:32 +0200

    added basic search functionality using Axios and Api

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#current-weather-city");
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "cf5fo805a314e7b0e5d7t87febb8a29a";
+  let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&unit=metric`;
+  axios.get(apiURL).then(refreshWeather);
+}
+
 function searchEngine(event) {
   event.preventDefault();
   let searchFormInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#current-weather-city");
-  cityElement.innerHTML = searchFormInput.value;
+  searchCity(searchFormInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
